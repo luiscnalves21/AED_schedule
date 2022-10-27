f1c55f9944a2e32573b942bf91cb68141403b5da
Luís Alves <luiscnalves@gmail.com>
2022-10-27 17:16:37 +0100

    Created directories

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
+#include <iostream>
 #include "student.h"
 #include "ucturma.h"
 #include "turmah.h"
-#include<set>
-#include<queue>
+#include <set>
+#include <queue>
 
 int main(){
-    set<Estudante> students;
-    vector<turmaH> horario;
+    set<Student> students;
+    vector<TurmaH> horario;
 
     return 0;
 }
