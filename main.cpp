67d09636d1cf1c4dd47197fbe87511f42befaaff
Luís Alves <luiscnalves@gmail.com>
2022-10-29 08:59:04 +0100

    Include changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "student.h"
-#include "ucturma.h"
-#include "turmah.h"
+#include "./include/student.h"
+#include "./include/ucturma.h"
+#include "./include/turmah.h"
 #include <set>
 #include <queue>
 
